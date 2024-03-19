be08e1573c5d084b157f5b19cefcc0ef21766aaa
Danz.Code <danzprogdanz@gmail.com>
2024-03-19 13:09:32 -0300

    init components elements

---
+++ front/src/components/ModalEditEmployeeComponent/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+interface IProps {
+
+}
+
+const ModalEditEmployeeComponent: React.FC<IProps> = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default ModalEditEmployeeComponent;
---
+++ front/src/components/TableComponent/index.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+interface TableComponentProps {
+  
+}
+
+const TableComponent: React.FC<TableComponentProps> = () => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default TableComponent;
