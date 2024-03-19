ce87dd71e11d49a85a83ef32b2b2d463db8f320a
Danz.Code <danzprogdanz@gmail.com>
2024-03-19 15:35:01 -0300

    implement header component

--- front/src/components/HeaderComponent/index.tsx
+++ front/src/components/HeaderComponent/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
+import React, { ReactNode, useState, useEffect } from 'react';
+import { HeaderStyled, HeaderTitle } from './styled';
 
 const HeaderComponent: React.FC = () => {
   
   return (
     <>
+      <HeaderStyled>
+        <HeaderTitle>N697 - Desenvolvimento de Software em Nuvem</HeaderTitle>
+      </HeaderStyled>
     </>
   );
 };
