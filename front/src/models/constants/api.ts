ab31f7549225d20970afd9ca217969442aaf981e
Danz.Code <danzprogdanz@gmail.com>
2024-03-19 13:23:26 -0300

    backend connection variables

---
+++ front/src/models/constants/api.ts
@@ -0,0 +1,6 @@
+export const API_URL = 'https://parseapi.back4app.com/classes/Employee';
+export const HEADERS = {
+  'X-Parse-Application-Id': 'nzrIUL3JXAGgGIKxS84qtbgtmdD3dvhPAasOgpPN',
+  'X-Parse-REST-API-Key': 'xLSr2DgEemxvnwyYrm3qWxjLEhsHWgoCQ9x9tJZW',
+  'Content-Type': 'application/json'
+};
