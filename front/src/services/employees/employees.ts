fe2a599ea90afb7c0543482f3242e4a56b0ee655
Anderson Freitas <andersonfreitas21@gmail.com>
2024-03-19 13:52:54 -0300

    Create employee

--- front/src/services/employees/employees.ts
+++ front/src/services/employees/employees.ts
@@ -3,3 +3,13 @@ import { IDataFromBack } from "../../interfaces/employee";
 import { API_URL, HEADERS } from "../../models/constants/api";
 
 
+export const createEmployee = async (createData: any) => {
+    try {
+      const response = await axios.post(API_URL, createData, { headers: HEADERS });
+      //console.log('Employee added:', response.data);
+      return response.data; // Optionally return the created employee data
+    } catch (error) {
+      console.error('Error adding employee:', error);
+      throw error; // Rethrow the error to handle it elsewhere if needed
+    }
+  };
