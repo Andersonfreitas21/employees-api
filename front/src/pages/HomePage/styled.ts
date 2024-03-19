1fed67a62179c9337ff65bf001c9f0ee1ea74789
Danz.Code <danzprogdanz@gmail.com>
2024-03-19 13:24:01 -0300

    others initial setup

--- front/src/pages/HomePage/styled.ts
+++ front/src/pages/HomePage/styled.ts
@@ -1,4 +1,4 @@
-+import styled from 'styled-components';
+import styled from 'styled-components';
 import { colors } from '../../assets/styles/colors';
 
 export const RootPageLoyoutStyled = styled.div`
